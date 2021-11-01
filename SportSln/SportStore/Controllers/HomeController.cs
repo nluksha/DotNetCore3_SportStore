a8f07f8f1049eeae3aa65ef4dc8752703045b312
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-01 20:19:34 +0300

    Add pagination

--- SportSln/SportStore/Controllers/HomeController.cs
+++ SportSln/SportStore/Controllers/HomeController.cs
@@ -4,18 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SportStore.Models;
+using SportStore.Models.ViewModels;
 
 namespace SportStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int MyProperty { get; set; }
+
+        public int PageSize { get; set; } = 4;
 
         public HomeController(IStoreRepository repository)
         {
             this.repository = repository;
         }
 
-        public IActionResult Index() => View(repository.Products);
+        public IActionResult Index(int page = 1)
+        {
+            var productsList = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(productsList);
+        }
     }
 }
---
+++ SportSln/SportStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportStore.Models.ViewModels;
+
+namespace SportStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper: TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            var result = new TagBuilder("div");
+
+            for (int i = 1; i < PageModel.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
