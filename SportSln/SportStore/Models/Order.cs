50c4a0c11c95835fc4a672d95611bd9dad641fad
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-04 09:54:05 +0300

    Add Shipped orders migrations

--- SportSln/SportStore/Models/Order.cs
+++ SportSln/SportStore/Models/Order.cs
@@ -27,5 +27,7 @@ namespace SportStore.Models
         [Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
         public bool GiftWrap { get; set; }
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
